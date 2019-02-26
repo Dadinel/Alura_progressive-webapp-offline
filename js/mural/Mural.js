bb9c5eefd80346e01b1ace8ef1b8007e4e9dbecb
Daniel Mendes De Melo Sousa <daniel.mendes@sp01.local>
2019-02-26 13:31:41 -0300

    Criação do manifest para utilização da app offline e alteração para gravar os dados no local storage

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,20 +1,64 @@
 const Mural = (function(_render, Filtro){
     "use strict"
-    let cartoes = []
+
+    let cartoes = pegaCartoesUsuario();
+
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+    render();
 
     Filtro.on("filtrado", render)
 
+    function preparaCartao(cartao) {
+        cartao.on("mudanca.**", salvaCartoes);
+        cartao.on("remocao", () => {
+            cartoes = cartoes.slice(0);
+            cartoes.splice(cartoes.indexOf(cartao),1);
+            salvaCartoes();
+            render();
+        });
+    }
+
+    function pegaCartoesUsuario() {
+        let cartoesLocal = JSON.parse(localStorage.getItem("cards_"+usuario));
+
+        if(cartoesLocal) {
+            return cartoesLocal.map(cartaoLocal => {
+                let cartao = new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo);
+                preparaCartao(cartao);
+                return cartao;
+            });
+        } else {
+            return [];
+        }
+    }
+
+    function salvaCartoes() {
+        localStorage.setItem("cards_"+usuario, JSON.stringify(
+            cartoes.map( cartao => ({conteudo: cartao.conteudo, tipo: cartao.tipo}) )
+        ));
+    }
+
+    login.on("login", () => {
+        cartoes = pegaCartoesUsuario();
+        render();
+    });
+
+    login.on("logout", () => {
+        cartoes = [];
+        render();
+    });
+
     function adiciona(cartao){
-        cartoes.push(cartao)
-        cartao.on("mudanca.**", render)
-        cartao.on("remocao", ()=>{
-            cartoes = cartoes.slice(0)
-            cartoes.splice(cartoes.indexOf(cartao),1)
+        if(logado) {
+            cartoes.push(cartao)
+            salvaCartoes();
+            cartao.on("mudanca.**", render)
+            preparaCartao(cartao);
             render()
-        })
-        render()
-        return true
+            return true
+        } else {
+            alert("Você não está logado");
+        }
     }
 
     return Object.seal({
